eb7166fed3da2015329661fb11f93eac3c822ba6
Tauseef Ahamed <tauseefahamed14@gmail.com>
2024-12-28 15:58:09 +0530

    feat - register screen ui completed

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,3 +1,5 @@
+import 'package:edu_app/features/auth/views/screens/login.dart';
+import 'package:edu_app/features/auth/views/screens/register.dart';
 import 'package:edu_app/features/auth/views/screens/welcome.dart';
 import 'package:go_router/go_router.dart';
 
@@ -7,5 +9,17 @@ GoRoute homeRoute = GoRoute(
   builder: (context, state) => WelcomeScreen(),
 );
 
+GoRoute registerRoute = GoRoute(
+  path: '/register',
+  name: 'Register',
+  builder: (context, state) => RegisterScreen(),
+);
+
+GoRoute loginRoute = GoRoute(
+  path: '/login',
+  name: 'Login',
+  builder: (context, state) => LoginScreen(),
+);
+
 // Route list
-List<GoRoute> routes = [homeRoute];
+List<GoRoute> routes = [homeRoute, registerRoute, loginRoute];
